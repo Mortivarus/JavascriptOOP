0888fd53a9b91934c607863a7b3fafada96398f0
Mortivarus <58182213+Mortivarus@users.noreply.github.com>
2021-02-25 13:05:45 +0100

    Afronding deel 2

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
-//Make a circle class
+//Make a parent class Shape
+class Shape{
+    constructor(color, name){
+        this._color = color;
+        this._name = name;
+    }
+    get name(){
+        return this._name
+    }
 
-class Circle {
+    set name(x){
+        this._name = x
+    }
+
+    get color(){
+        return this._color
+    }
+
+    set color(x){
+        this._color = x   
+    }
+}
+
+//Make a circle class
+class Circle extends Shape{
     constructor(radius, color, name){
+        super(color, name);
         this.radius = radius;
-        this.color = color;
-        this.name = name;
     }
     radius(){
         return this.radius
@@ -12,32 +33,27 @@ class Circle {
     area(){
         return Math.PI * Math.pow(this.radius, 2)
     }
-
-    name(){
-        return this.name
-    }
 }
 
 //Make three circles
-const circleBlue = new Circle(1.5, 'blue', 'Harambe')
+const circleBlue = new Circle(1.5, 'blue', 'blauwe cirkel')
 
-const circleRed = new Circle(10, 'red', 'Charlie')
+const circleRed = new Circle(10, 'red', 'rode cirkel')
 
-const circleYellow = new Circle(20, 'yellow', 'Richard')
+const circleYellow = new Circle(20, 'yellow', 'gele cirkel')
 
 //Print the circles on the screen
-document.getElementById("circleBlue").innerHTML = ` De blauwe cirkel heeft een oppervlak van ${circleBlue.area().toFixed(2)}`
+document.getElementById("circleBlue").innerHTML = ` De ${circleBlue.name} heeft een oppervlak van ${circleBlue.area().toFixed(2)}`
 
-document.getElementById("circleRed").innerHTML = ` De rode cirkel heeft een oppervlak van ${circleRed.area().toFixed(2)}`
+document.getElementById("circleRed").innerHTML = ` De ${circleRed.name} heeft een oppervlak van ${circleRed.area().toFixed(2)}`
 
-document.getElementById("circleYellow").innerHTML = ` De gele cirkel heeft een oppervlak van ${circleYellow.area().toFixed(2)}`
+document.getElementById("circleYellow").innerHTML = ` De ${circleYellow.name} heeft een oppervlak van ${circleYellow.area().toFixed(2)}`
 
 //Make a square class
-class Square{
+class Square extends Shape{
     constructor(size, color, name){
+        super(color, name);
         this.size = size;
-        this.color = color;
-        this.name = name;
     }
     size(){
         return this.size
@@ -46,33 +62,28 @@ class Square{
     area(){
         return Math.pow(this.size, 2)
     }
-    
-    name(){
-        return this.name
-    }
 }
 
 //Make three squares
-const squareBlack = new Square(2, 'black', "Caspar")
+const squareBlack = new Square(2, 'black', "zwarte vierkant")
 
-const squareGreen = new Square(5, 'green', "Balthasar")
+const squareGreen = new Square(5, 'green', "groene vierkant")
 
-const squareOrange = new Square(100, 'orange', "Melchior")
+const squareOrange = new Square(100, 'orange', "oranje vierkant")
 
 //Print the squares on the screen
-document.getElementById("squareBlack").innerHTML = `De zwarte vierkant heeft een oppervlak van ${squareBlack.area()}`
+document.getElementById("squareBlack").innerHTML = `De ${squareBlack.name} heeft een oppervlak van ${squareBlack.area()}`
 
-document.getElementById("squareGreen").innerHTML = `De groene vierkant heeft een oppervlak van ${squareGreen.area()}`
+document.getElementById("squareGreen").innerHTML = `De ${squareGreen.name} heeft een oppervlak van ${squareGreen.area()}`
 
-document.getElementById("squareOrange").innerHTML = `De oranje vierkant heeft een oppervlak van ${squareOrange.area()}`
+document.getElementById("squareOrange").innerHTML = `De ${squareOrange.name} heeft een oppervlak van ${squareOrange.area()}`
 
 //Make a rectangle class
-class Rectangle{
+class Rectangle extends Shape{
     constructor(width, height, color, name){
+        super(color, name);
         this.width = width;
         this.height = height;
-        this.color = color;
-        this.name = name;
     }
     width(){
         return this.width
@@ -88,16 +99,15 @@ class Rectangle{
 }
 
 //Make three rectangles
+const rectangleBlack = new Rectangle(2, 5, 'black', 'zwarte rechthoek')
 
-const rectangleBlack = new Rectangle(2, 5, 'black', 'Kwik')
+const rectangleGreen = new Rectangle(5, 10, 'green', 'groene rechthoek')
 
-const rectangleGreen = new Rectangle(5, 10, 'green', 'Kwek')
-
-const rectangleOrange = new Rectangle(100, 100, 'orange', 'Kwak')
+const rectangleOrange = new Rectangle(100, 100, 'orange', 'oranje rechthoek')
 
 //Print the squares on the screen
-document.getElementById("rectangleBlack").innerHTML = `De zwarte rechthoek heeft een oppervlak van ${rectangleBlack.area()}`
+document.getElementById("rectangleBlack").innerHTML = `De ${rectangleBlack.name} heeft een oppervlak van ${rectangleBlack.area()}`
 
-document.getElementById("rectangleGreen").innerHTML = `De groene rechthoek heeft een oppervlak van ${rectangleGreen.area()}`
+document.getElementById("rectangleGreen").innerHTML = `De ${rectangleGreen.name} heeft een oppervlak van ${rectangleGreen.area()}`
 
-document.getElementById("rectangleOrange").innerHTML = `De oranje rechthoek heeft een oppervlak van ${rectangleOrange.area()}`
+document.getElementById("rectangleOrange").innerHTML = `De ${rectangleOrange.name} heeft een oppervlak van ${rectangleOrange.area()}`
